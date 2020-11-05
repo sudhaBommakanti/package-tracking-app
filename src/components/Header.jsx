12d66e91cb810d1128512654cb74beb3933a231a
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-05 06:41:24 +0100

    created header for the search-page

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/modules/header.css';
+
+export default function Header() {
+    return(
+        <header>
+            <div className= "header">
+            <form className="example">
+                <input type="text" placeholder="Search.." name="search2" />
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+               
+            </div>
+        </header>
+    );
+}
---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header';
+
+export default function SearchPage() {
+    return (
+        <div>
+            <Header />
+            <h1>Search Page</h1>
+        </div>
+    );
+}
